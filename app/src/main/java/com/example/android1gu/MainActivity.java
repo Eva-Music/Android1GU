54e481a0b19aecdafe9bbade6701e0f948c5ebb3
Eva-Music <crazyolla@rambler.ru>
2019-02-25 11:15:38 +0300

    UI changes: add Checkboxes, ImageViews, new logic

--- app/src/main/java/com/example/android1gu/MainActivity.java
+++ app/src/main/java/com/example/android1gu/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.android1gu;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import javax.xml.datatype.Duration;
+import static com.example.android1gu.R.string.*;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "TAG";
+    public static final String HUM = "hum";
+    public static final String WIND = "wind";
+    public static final String RESULT_COUNTER_TEXT = "text";
+    public static final int REQUEST_CODE = 10;
+    final ActivitySaver activitySaver = ActivitySaver.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,51 +30,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String currentInstance;
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             currentInstance = "Первый запуск";
-        } else  {
+        } else {
             currentInstance = "Повторный запуск";
         }
 
         Log.d(TAG, "onCreate");
-        Toast.makeText(this.getApplicationContext(), currentInstance+ " onCreate", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.getApplicationContext(), currentInstance + " onCreate", Toast.LENGTH_SHORT).show();
+
+        downloadNextActivity();
+        setText();
+        incrementButtonJob();
+    }
+
+    public void downloadNextActivity() {
+        final EditText editText = findViewById(R.id.edit_city);
+        Button buttonResult = findViewById(R.id.button_result);
+        final CheckBox checkBoxHumidity = findViewById(R.id.checkbox_humidity);
+        final CheckBox checkBoxWind = findViewById(R.id.checkbox_wind);
+
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activitySaver.setCity(editText.getText().toString());
+
+                final Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+                if (checkBoxHumidity.isChecked()) {
+                    intent.putExtra(HUM, true);
+                }
+                if (checkBoxWind.isChecked()) {
+                    intent.putExtra(WIND, true);
+                }
+
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @SuppressLint("DefaultLocale")
+    public void setText() {
+        TextView resultView = findViewById(R.id.text_view_quantity);
+        resultView.setText(String.format("%s %d %s", getResources().getString(pushed),
+                activitySaver.getCounter(), getResources().getString(times)));
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-        Toast.makeText(this.getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
-
+    public void incrementButtonJob() {
+        final Button incrementButton = findViewById(R.id.button_counter);
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activitySaver.incrementCounter();
+                setText();
+            }
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-        Toast.makeText(this.getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
-    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-        Toast.makeText(this.getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
-    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (data != null) {
+            Toast.makeText(this.getApplicationContext(), data.getStringExtra(RESULT_COUNTER_TEXT) + " " +
+                    activitySaver.getCity(), Toast.LENGTH_SHORT).show();
+        }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-        Toast.makeText(this.getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-        Toast.makeText(this.getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
     @Override
